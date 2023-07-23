e98e86ee9207869c68c79eb706fb1022e26248a7
facundo Lescano <facundoleonellescano@gmail.com>
2023-07-23 14:50:28 -0300

    add views

--- app_agenda_project/diary/views.py
+++ app_agenda_project/diary/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
     return HttpResponse("hola")
+
+def profile(request):
+    pass
+
+def login(request):
+    pass
+
+def close_session(request):
+    pass
+
+def signup(request):
+    pass
+
+def create_task(request):
+    pass
+
+def delete_task(request):
+    pass
